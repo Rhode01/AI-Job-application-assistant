5a8299ee3410ca941b8b8c6f2c214000427fcb6f
Rhode01 <Rhodrickmhone@gmail.com>
2025-02-03 17:53:00 +0200

    Evertything working now remaning is trying to automate the whole process now

--- utils.py
+++
@@ -1,6 +0,0 @@
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-def load_cv_file() -> dict:
-    documents = SimpleDirectoryReader("data").load_data()
-    index = VectorStoreIndex.from_documents(documents)
-    query_engine = index.as_query_engine()
-    return {"query_engine": query_engine}
