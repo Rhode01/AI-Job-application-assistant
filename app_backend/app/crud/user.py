5e7549e09a50f1b6a31d6fcfbd015d722473f781
Rhode01 <Rhodrickmhone@gmail.com>
2025-02-12 12:19:08 +0200

    Config working smoothly

--- app_backend/app/crud/user.py
+++ app_backend/app/crud/user.py
@@ -1,6 +1,6 @@
-from db.base import Base
-from models.user import User
-from crud.base import CRUDBase
+from app_backend.app.db.base import Base
+from app_backend.app.models.user import User
+from app_backend.app.crud.base import CRUDBase
 
 class CRUDUser([CRUDBase]):
      def __init__(self):
