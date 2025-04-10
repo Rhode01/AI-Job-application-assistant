2388c5a4d33d45c10d50abc84deeb7ab7302b34d
Rhode01 <Rhodrickmhone@gmail.com>
2025-04-10 12:07:56 +0200

    .

--- front_end/jas/src/ENDPOINTS/index.js
+++
@@ -1,2 +0,0 @@
-export const HOMEURL ="http://localhost:8000"
-export const CVUPLOAD ="http://localhost:8000/upload_cv"
---
+++ front_end/jas/src/api/api.js
@@ -0,0 +1,7 @@
+const api ={
+    endpoints:{
+        HOMEURL :"http://localhost:8000",
+        CVUPLOAD:"http://localhost:8000/upload_cv"
+    }
+}
+export default api
